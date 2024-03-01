94822bfbedbf05739725369cbfea021405546c64
takdlapowiatugdanskiego <takdlapowiatugdanskiego@gmail.com>
2024-03-01 09:18:32 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <b>KOMITET WYBORCZY WYBORCÓW TAK! DLA POWIATU GDAŃSKIEGO</b>
+        <br />
+        Wybory samorządowe 2024
+      </header>
+      <div className="App-body">
+        <a
+          href="https://docs.google.com/document/d/1xR3WpDL6s1PPk-t5GYRjnz1CLLULUegyK3F4JFxY9Rs/edit?usp=sharing"
+          target="blank"
+        >
+          Rejestr zaciągniętych kredytów
+        </a>
+        <br />
+        <a
+          href="https://docs.google.com/document/d/1bnQ-FUSfyOVkHVM0c6an8gQikNrkI5k3kT9AUIVdDqo/edit?usp=sharing"
+          target="blank"
+        >
+          Rejestr wpłat o wartości przekraczającej łącznie 4242,00 zł
+        </a>
+      </div>
+      <footer className="App-footer">
+        <div>
+          <p>
+            <b>KOMITET WYBORCZY WYBORCÓW TAK! DLA POWIATU GDAŃSKIEGO</b>
+          </p>
+          <p>
+            ul. Bajkowa 6<br />
+            Piekło Dolne
+            <br />
+            83-047 Przywidz
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
